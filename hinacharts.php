13131f730d6fa5497d1e03f7126841c4696e5076
Hina Lowell <hina.lowell.0202@gmail.com>
2022-07-18 08:45:37 +0900

    Update comments

--- hinacharts.php
+++ hinacharts.php
@@ -1,15 +1,23 @@
 <?php
+
 /**
+ * The plugin entrypoint
+ *
+ * @package   HinaCharts
+ * @author    Hina Lowell
+ * @copyright 2022 Hina Lowell
+ * @license   GPL-3.0
+ *
+ * @wordpress-plugin
  * Plugin Name: HinaCharts
  * Plugin URI:  https://github.com/HinaLowell/hinacharts
  * Description: Extract values ​​from a database and draw charts.
  * Version:     0.0.0
  * Author:      Hina Lowell
  * Author URI:  https://github.com/HinaLowell
- * License:     GPLv3
+ * License:     GPL-3.0
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
- * Update URI:  https://example.com/hinacharts
  * Text Domain: hinacharts
  * Domain Path: /languages
+ * Update URI:  https://example.com/hinacharts
  */
-?>
--- uninstall.php
+++ uninstall.php
@@ -1,17 +1,25 @@
 <?php
-// if uninstall.php is not called by WordPress, die
+
+/**
+ * The plugin uninstaller
+ *
+ * @package   HinaCharts
+ * @author    Hina Lowell
+ * @copyright 2022 Hina Lowell
+ * @license   GPL-3.0
+ */
+
 if (!defined("WP_UNINSTALL_PLUGIN")) {
     die;
 }
- 
+
 // $option_name = "wporg_option";
- 
+
 // delete_option($option_name);
- 
+
 // for site options in Multisite
 // delete_site_option($option_name);
- 
+
 // drop a custom database table
 // global $wpdb;
 // $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}mytable");
-?>
