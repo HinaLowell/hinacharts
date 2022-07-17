bb59bb0c0f13fe111bab64bf0aba78e5cf2bdd0c
Hina Lowell <hina.lowell.0202@gmail.com>
2022-07-17 17:26:19 +0900

    Add main php

---
+++ hinacharts.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: HinaCharts
+ * Plugin URI:  https://hinafuturediary.jp
+ * Description: Extract values ​​from a database and draw charts.
+ * Version:     0.0.0
+ * Author:      Hina Lowell
+ * Author URI:  https://hinafuturediary.jp/author/hina
+ * License:     GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Update URI:  https://hinafuturediary.jp/plugins/hinacharts
+ * Text Domain: hinacharts
+ * Domain Path: /languages
+ */
+?>
